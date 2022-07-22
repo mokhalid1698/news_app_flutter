1fc6dbb9b9251f677c30869e0b622396ee650ad6
mohamed khalid <63412211+mokhalid1698@users.noreply.github.com>
2022-07-22 16:33:35 +0200

    test_mo

---
+++ lib/shared/styles/themes.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+ThemeData darkTheme =  ThemeData(
+  scaffoldBackgroundColor: Colors.black26,
+  primarySwatch: Colors.teal,
+  disabledColor: Colors.white60,
+  inputDecorationTheme: InputDecorationTheme(
+      border: const OutlineInputBorder(),
+      disabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12)),
+      enabledBorder: const OutlineInputBorder(
+        borderSide: BorderSide(
+
+        ),
+      )),
+  appBarTheme: const AppBarTheme(
+      color: Colors.black,
+      iconTheme: IconThemeData(color: Colors.white),
+      elevation: 0.0,
+      systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Colors.black,
+          statusBarIconBrightness: Brightness.light)),
+  textTheme: const TextTheme(
+      bodyText1: TextStyle(
+        fontSize: 16, overflow: TextOverflow.ellipsis,
+        color: Colors.white,
+        // fontSize: 40,
+        // fontWeight: FontWeight.bold,
+      ),
+      bodyText2: TextStyle(
+        fontSize: 10, overflow: TextOverflow.ellipsis,
+        color: Colors.white,
+        // fontSize: 40,
+        // fontWeight: FontWeight.bold,
+      ),
+      headline1: TextStyle(
+        fontSize: 14,
+        color: Colors.white,
+        overflow: TextOverflow.ellipsis,
+        fontWeight: FontWeight.bold,
+      )),
+);
+ThemeData  lightTheme =  ThemeData(
+  inputDecorationTheme: InputDecorationTheme(
+      border: const OutlineInputBorder(),
+      disabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12)),
+      enabledBorder: const OutlineInputBorder(
+        borderSide: BorderSide(
+        ),
+      )),
+  primarySwatch: Colors.teal,
+
+  appBarTheme: const AppBarTheme(
+      color: Colors.white,
+      iconTheme: IconThemeData(color: Colors.black),
+      elevation: 0.0,
+      systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Colors.white,
+          statusBarIconBrightness: Brightness.dark)),
+  textTheme: const TextTheme(
+      bodyText2: TextStyle(
+        fontSize: 10, overflow: TextOverflow.ellipsis,
+        color: Colors.black,
+        // fontSize: 40,
+        // fontWeight: FontWeight.bold,
+      ),
+      bodyText1:
+      TextStyle(fontSize: 16, overflow: TextOverflow.ellipsis
+        // color: Colors.white,
+        // fontSize: 40,
+        // fontWeight: FontWeight.bold,
+      ),
+      headline1: TextStyle(
+        color: Colors.black,
+        fontSize: 14,
+        overflow: TextOverflow.ellipsis,
+        fontWeight: FontWeight.bold,
+      )),
+);
