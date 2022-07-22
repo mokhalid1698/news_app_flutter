01f552b2349de331edbf0a82fab5d1c49165ab86
mohamed khalid <63412211+mokhalid1698@users.noreply.github.com>
2022-07-22 18:05:18 +0200

    ShopApp

--- lib/modules/shop_app/ProductsScreen/ProductsScreen.dart
+++ lib/modules/shop_app/ProductsScreen/ProductsScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProductsScreen extends StatelessWidget {
@@ -5,10 +6,78 @@ class ProductsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Center(
-        child: Text(
-          "ProductsScreen",
-          style: TextStyle(fontSize: 40),
-        ));
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+
+
+
+
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+
+
+              const Text("Categories",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30)),
+              const SizedBox(height: 15,),
+              SizedBox(
+                height: 180,
+                // color: Colors.black45,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                    physics: BouncingScrollPhysics(),
+                    itemBuilder: (context, index) => BiuldFormCategories(),
+                    separatorBuilder: (context, index) => const SizedBox(
+                          width: 50.0,
+                        ),
+                    itemCount: 10),
+              ),
+
+
+
+            ],
+          ),
+        ),
+
+
+
+
+
+      ],
+    );
   }
+
+  // ignore: non_constant_identifier_names
+  Widget BiuldFormCategories() => SizedBox(
+    height: 100,
+    width: 100,
+    child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            CircleAvatar(
+
+              radius: 60,
+              backgroundImage: NetworkImage(
+                  "https://student.valuxapps.com/storage/uploads/categories/16301438353uCFh.29118.jpg"),
+            ),
+            SizedBox(
+              height: 7,
+            ),
+            Text(
+              "electrionic devices",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+  );
 }
+
+// https://student.valuxapps.com/storage/uploads/products/1615440322npwmU.71DVgBTdyLL._SL1500_.jpg
